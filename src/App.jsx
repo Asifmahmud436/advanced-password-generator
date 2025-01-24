55142425bf695ca59ca334a6126574b48f799629
Zettex <safaandsafa4@gmail.com>
2025-01-24 14:17:27 +0600

    the skeleton is set

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,38 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [password, setPassword] = useState('')
+  const [length,setLength] = useState(6)
+  const [isNumber,setIsNumber] = useState(false)
+  const [isChar,setIsChar] = useState(false)
+
+
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <input type="text" name="" id="" readOnly value={password}/>
+        <button type="button">Copy</button>
+        <br />
+        <label htmlFor="number">Number</label>
+        <input type='checkbox' name="" id="number" />
+        <br />
+        <label htmlFor="char">Special Character</label>
+        <input type='checkbox' name="" id="char" />
+        <br />
+        <label htmlFor="range">{length}</label>
+        <input 
+        type="range" 
+        name="" 
+        id="range" 
+        min='6' 
+        max='35'
+        onChange={(e)=>setLength(e.target.value)}
+        defaultValue={length}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
