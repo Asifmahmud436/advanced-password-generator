f3ce8728b9810a1e382ab972aec2388387e82a5f
Zettex <safaandsafa4@gmail.com>
2025-01-24 14:37:44 +0600

    can generate passwords now

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useCallback, useEffect, useRef } from 'react'
 
 import './App.css'
 
@@ -7,8 +7,34 @@ function App() {
   const [length,setLength] = useState(6)
   const [isNumber,setIsNumber] = useState(false)
   const [isChar,setIsChar] = useState(false)
+  
 
+  const passGenerator = useCallback(()=>{
+    let pass = ''
+    let str = 'qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM'
+    
+    if(isChar) str+= '!@#$%^&*()_+~`'
+    if(isNumber) str+= '1234567890'
 
+    for(let i = 0; i< length; i++){
+      pass += str.charAt(Math.floor(Math.random()*str.length + 1)) 
+    }
+
+    setPassword(pass)
+
+  },[password,length,isNumber,isChar])
+
+  useEffect(()=>{
+    passGenerator()
+  },[length,isChar,isNumber])
+  
+  function handleNumber(){
+    setIsNumber((prev)=>!prev)
+  }
+
+  function handleChar(){
+    setIsChar((prev)=>!prev)
+  }
 
   return (
     <>
@@ -16,11 +42,21 @@ function App() {
         <input type="text" name="" id="" readOnly value={password}/>
         <button type="button">Copy</button>
         <br />
+
         <label htmlFor="number">Number</label>
-        <input type='checkbox' name="" id="number" />
+        <input 
+        type='checkbox' 
+        name="" 
+        id="number" 
+        onChange={handleNumber}/>
         <br />
+
         <label htmlFor="char">Special Character</label>
-        <input type='checkbox' name="" id="char" />
+        <input 
+        type='checkbox' 
+        name="" 
+        id="char"  
+        onChange={handleChar}/>
         <br />
         <label htmlFor="range">{length}</label>
         <input 
